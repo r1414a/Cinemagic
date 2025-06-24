38822bc4bd933490ee3954fa000bb94ecba46b66
r1414a <rupeshchincholkar14@gmail.com>
2025-06-24 11:33:02 +0530

    changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,6 +9,6 @@ router.post('/send-otp', sendOTPForAuthentication);
 router.post('/verify-otp', verifyOTPForAuthentication);
 router.post('/google', authenticateGoogleUser);
 router.get('/checkUserAuth', checkIfUserHasAccount)
-router.post('/logout', logoutUser);
+router.get('/logout', logoutUser);
 
 export default router;
