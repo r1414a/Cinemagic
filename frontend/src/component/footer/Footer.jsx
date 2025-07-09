309fc20f75e4a065c615143319943c58d371ba4d
r1414a <rupeshchincholkar14@gmail.com>
2025-07-09 11:36:39 +0530

    changes

--- frontend/src/component/footer/Footer.jsx
+++ frontend/src/component/footer/Footer.jsx
@@ -2,15 +2,16 @@ import { FaRegCopyright } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLink } from "react-icons/fa";
+import { Link } from "react-router";
 
 export default function  Footer(){
     return(
         <footer className="bg-dyellow py-20 space-y-5">
             <div >
                 <ul className="flex justify-center gap-4">
-                    <li className="bg-blue p-2"><FaLinkedin className="text-xl text-white"/></li>
-                    <li className="bg-blue p-2"><FaGithub className="text-xl text-white"/></li>
-                    <li className="bg-blue p-2"><FaLink className="text-xl text-white"/></li>
+                    <Link to={'https://www.linkedin.com/in/rupesh-chincholkar-08bb7612b/'} target="_blank" className="bg-blue p-2"><FaLinkedin className="text-xl text-white"/></Link>
+                    <Link to={'https://github.com/r1414a/'} target="_blank" className="bg-blue p-2"><FaGithub className="text-xl text-white"/></Link>
+                    <Link to={'https://r1414a.github.io/portfolio-website/'} target="_blank" className="bg-blue p-2"><FaLink className="text-xl text-white"/></Link>
                 </ul>
             </div>
             <div>
